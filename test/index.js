23496935f84b38bd986ccb651c7e566fd4d0d397
clarakosi <candrewwani@gmail.com>
2018-10-29 17:42:21 -0400

    updated dependencies and small change to a test
    Removed .only from test in test/features/router
    Updated patch number
    Removed jsint and jscs

--- test/index.js
+++ test/index.js
@@ -1,8 +1,4 @@
 'use strict';
 
-// Run jshint as part of normal testing
-require('mocha-jshint')();
-// Run jscs as part of normal testing
-require('mocha-jscs')();
 // Run eslint as part of normal testing
 require('mocha-eslint')([ './lib', './index.js' ]);
